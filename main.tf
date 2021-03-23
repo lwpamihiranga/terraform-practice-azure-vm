d2012026d5b1ab0855e85fb56f2a67ecd1aa5f28
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-03-23 15:01:36 +0530

    remove unnecessary commented attributes

--- main.tf
+++ main.tf
@@ -62,21 +62,14 @@ resource "azurerm_network_security_group" "practice_security_group" {
 
   security_rule {
     access = "Allow"
-    #description = "value"
     destination_address_prefix = "*"
-    # destination_address_prefixes = [ "value" ]
-    # destination_application_security_group_ids = [ "value" ]
     destination_port_range = "22"
-    # destination_port_ranges = [ "value" ]
     direction = "Inbound"
     name = "SSH"
     priority = 1001
     protocol = "TCP"
     source_address_prefix = "*"
-    # source_address_prefixes = [ "value" ]
-    # source_application_security_group_ids = [ "value" ]
     source_port_range = "*"
-    # source_port_ranges = [ "value" ]
   } 
 
   tags = {
