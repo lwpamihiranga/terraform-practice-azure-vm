892f6c63452f6506bed5a0c629bbe891a4d96d9f
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-03-23 14:06:31 +0530

    configure public ip address

--- main.tf
+++ main.tf
@@ -41,3 +41,15 @@ resource "azurerm_subnet" "practice_subnet" {
   virtual_network_name  = azurerm_virtual_network.practice_network.name
   address_prefixes      = [ "10.0.2.0/24" ]
 }
+
+# Create public IP address
+resource "azurerm_public_ip" "practice_public_ip" {
+  name = "terraform-public-ip"
+  location = azurerm_resource_group.practice_resource_group.location
+  resource_group_name = azurerm_resource_group.practice_resource_group.name
+  allocation_method = "Dynamic"
+
+  tags = {
+    environment = "Terraform Practice"
+  }
+}
